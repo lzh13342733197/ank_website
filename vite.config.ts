23c1f1be889110fb8ec7c67aa087331a9c66c0c5
ik6 <10575553+zh12138111@user.noreply.gitee.com>
2026-01-21 11:32:53 +0800

    更改base

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,7 @@ import viteImagemin from 'vite-plugin-imagemin'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/ankibit_website/',
+ base: process.env.VITE_BASE,
   server: {
     port: 3345,
     host: '0.0.0.0',
