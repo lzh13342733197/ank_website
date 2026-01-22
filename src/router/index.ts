500a1986367dd29ae4de9fce9c9a0ddbba5788f4
lzh13342733197 <lzh13342733197@gmail.com>
2026-01-22 16:06:24 +0800

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,128 +1,49 @@
-import { createRouter, createWebHistory, createMemoryHistory,createWebHashHistory  } from 'vue-router'
-import { isMobileDevice } from '@/utils/utils'
-const isServer = import.meta.env.SSR
-const history = isServer
-  ? createMemoryHistory(import.meta.env.BASE_URL)
-  : createWebHashHistory(import.meta.env.BASE_URL)
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 const router = createRouter({
-  history,
+  history: createWebHashHistory('/ank_website/'),
   routes: [
-    {
-      path: '/',
-      redirect: (to?: any) => {
-        if (to.query.s) {
-          // 处理搜索参数
-          return { path: '/home', query: { s: to.query.s } }
-        }
-        return '/home'
-      },
-    },
     {
       path: '/',
       component: () => import('../layout/index.vue'),
       children: [
-        {
-          path: '',
-          redirect: () => {
-            return '/home'
-          },
-        },
-        {
-          path: 'home',
-          component: () => import('../layout/home.vue'),
-        },
-        {
-          path: 'product-detailInfo',
-          component: () => import('../layout/product-detailInfo.vue'),
-        },
-        {
-          path: 'Contact_us',
-          component: () => import('../layout/components/Contact_us.vue'),
-        },
-        {
+        { path: '', redirect: 'home' }, // 根路径重定向到 /home
+        { path: 'home', component: () => import('../layout/home.vue') },
+        { path: 'product-detailInfo', component: () => import('../layout/product-detailInfo.vue') },
+        { path: 'Contact_us', component: () => import('../layout/components/Contact_us.vue') },
+        { 
           path: 'AboutUs',
           component: () => import('../layout/AboutUsPage.vue'),
           children: [
-            {
-              path: 'patent',
-              component: () => import('../layout/components/ResearchDevelopment.vue'),
-            },
-            {
-              path: 'Credentials',
-              component: () => import('../layout/components/Credentials.vue'),
-            },
-            {
-              path: 'CompanyProfile',
-              component: () => import('../layout/components/aboutUs/CompanyProfile.vue'),
-            }
-            ,
-            {
-              path: 'DevelopmentCourse',
-              component: () => import('../layout/components/aboutUs/DevelopmentCourse.vue'),
-            },
-            {
-              path: 'CoreValue',
-              component: () => import('../layout/components/aboutUs/CoreValue.vue'),
-            },
-            {
-              path: 'Awards',
-              component: () => import('../layout/components/aboutUs/Awards.vue'),
-            }
-
+            { path: 'patent', component: () => import('../layout/components/ResearchDevelopment.vue') },
+            { path: 'Credentials', component: () => import('../layout/components/Credentials.vue') },
+            { path: 'CompanyProfile', component: () => import('../layout/components/aboutUs/CompanyProfile.vue') },
+            { path: 'DevelopmentCourse', component: () => import('../layout/components/aboutUs/DevelopmentCourse.vue') },
+            { path: 'CoreValue', component: () => import('../layout/components/aboutUs/CoreValue.vue') },
+            { path: 'Awards', component: () => import('../layout/components/aboutUs/Awards.vue') },
           ]
         },
-        {
-          path: 'NewsList',
-          component: () => import('../layout/NewsList.vue'),
-        },
-        {
-          path: 'ProductCenter',
-          component: () => import('../layout/ProductCenter.vue'),
-        },
-        {
-          path: 'CategoryList',
-          component: () => import('../layout/components/ProductCenter/CategoryList.vue'),
-        },
-        {
-          path: 'Factory',
-          redirect: '/Factory/Production',
+        { path: 'NewsList', component: () => import('../layout/NewsList.vue') },
+        { path: 'ProductCenter', component: () => import('../layout/ProductCenter.vue') },
+        { path: 'CategoryList', component: () => import('../layout/components/ProductCenter/CategoryList.vue') },
+        { 
+          path: 'Factory', 
+          redirect: 'Factory/Production',
           children: [
-            {
-              path: 'Production',
-              component: () => import('../layout/Factory.vue'),
-            },
-            {
-              path: 'Testing',
-              component: () => import('../layout/Testing.vue'),
-            },
-            {
-              path: 'Overview',
-              component: () => import('../layout/Overview.vue'),
-            }
+            { path: 'Production', component: () => import('../layout/Factory.vue') },
+            { path: 'Testing', component: () => import('../layout/Testing.vue') },
+            { path: 'Overview', component: () => import('../layout/Overview.vue') }
           ]
         },
-        {
-          path: 'Blog',
-          component: () => import('../layout/Blog.vue'),
-        }
+        { path: 'Blog', component: () => import('../layout/Blog.vue') },
       ],
     },
-    {
-      path: '/privacy',
-      component: () => import('@/components/privacy.vue'),
-    },
-    {
-      path: '/:pathMatch(.*)*',
-      component: () => import('../views/404.vue'),
-    },
+    { path: '/privacy', component: () => import('@/components/privacy.vue') },
+    { path: '/:pathMatch(.*)*', component: () => import('../views/404.vue') },
   ],
-  scrollBehavior(to, from, savedPosition) {
-    return {
-      top: 0,
-      behavior: 'smooth',
-    }
-  },
+  scrollBehavior() {
+    return { top: 0, behavior: 'smooth' }
+  }
 })
 
 export default router
